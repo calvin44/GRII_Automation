e1b885345398337f901370e92c99d71705bd3169
Calvin Fredicson <calvin.fredicson@gmail.com>
2024-12-09 20:59:04 +0800

    feat: add logo for favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,15 @@ import { Html, Head, Main, NextScript } from "next/document"
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" type="image/jpeg" href="/logo-grii-02.jpg" />
+        <link rel="apple-touch-icon" href="/logo-grii-02.jpg" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-title" content="Pengurus Ibadah" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="description" content="Apps to remind penatalayan" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <body>
         <Main />
         <NextScript />
